bab0eb49fe8c6de51832ccfa8e9cb221ea09879d
Mike Menza <menzascripting@gmail.com>
2018-03-04 21:12:44 -0500

    continued work on dev branch

---
+++ lib/Mnet/Log/Test.pm
@@ -0,0 +1,32 @@
+package Mnet::Opts::Set::Test;
+
+=head1 NAME
+
+Mnet::Opts::Set::Test
+
+=head1 SYNOPSIS
+
+Use this module to enable the --test option from the Mnet::Test module.
+
+This is meant to be used by distribution test scripts in order to filter out
+timestamps and other varying output from Mnet::Log entries.
+
+Refer to perldoc Mnet::Test and Mnet::Log for more information.
+
+=cut
+
+# required modules
+use warnings;
+use strict;
+
+=head1 SEE ALSO
+
+ Mnet
+ Mnet::Log
+ Mnet::Test
+
+=cut
+
+# normal end of package
+1;
+
